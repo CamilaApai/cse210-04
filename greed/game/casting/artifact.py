b0ab4cf28f757157ed7575cc7855d273386e3428
CamilaApai <Camilaapai@hotmail.com>
2022-10-09 04:17:57 -0300

    commit

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -1,12 +1,23 @@
 from game.casting.actor import Actor
 
 class Artifact(Actor):
+    """Outputs the game points. 
+    
+    The responsibility of Artifact is to calculate the game points.
 
+    Attributes:
+        _points (int): The game points
+    """
     def __init__(self):
+        """Constructs a new Artifact."""
         super().__init__()
         self._points = 0
 
-    def get_points(self):       
+    def get_points(self):   
+        """Calculates the points that the player receives based on what touches.
+        
+        Returns: The points
+        """    
         if (self.get_text() == "*"):
             self._points = 1
         else:
