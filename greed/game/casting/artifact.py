dab3f504f8304338de64b16217d4234763f5ece1
CamilaApai <113564236+CamilaApai@users.noreply.github.com>
2022-10-09 04:05:06 -0300

    Add files via upload

---
+++ greed/game/casting/artifact.py
@@ -0,0 +1,16 @@
+from game.casting.actor import Actor
+
+class Artifact(Actor):
+
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+
+    def get_points(self):       
+        if (self.get_text() == "*"):
+            self._points = 1
+        else:
+            self._points = -1
+
+        return self._points
+    
